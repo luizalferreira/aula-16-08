58b1e8948dd2c0cbef8161cac96f709c9041390c
luizalferreira <luizalf2006@gmail.com>
2022-08-23 08:45:56 -0300

    23-08

--- scripts.js
+++ scripts.js
@@ -13,6 +13,26 @@ $(document).ready(function() {
         $('#mensagem').append(frase)
     })
     $('#NOME').focusin(function() {
-        $('#mensagem').empty()
+            $('#mensagem').empty()
+        })
+        //dia 23/08
+
+    $('p').hide()
+    $('#btn-resumo').click(function(e) {
+        e.preventDefault()
+
+        // Criar uma variável para receber o status
+        let status = $(this).attr('data-status')
+        $('#btn-resumo').empty()
+
+        if (status == '1') {
+            $('#btn-resumo').append('Esconder resumo')
+            $('#btn-resumo').attr('data-status', '2')
+        } else {
+            $('#btn-resumo').append('Ver resumo')
+            $('#btn-resumo').attr('data-status', '1')
+        }
+
+        $('p').toggle(2000)
     })
 })
